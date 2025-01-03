58826c983fe6e5bb812a229f45481ee8e0bfcad1
vikasgodara290 <vikasgodara290@gmail.com>
2025-01-03 23:18:51 +0530

    Working on single question

--- src/App.jsx
+++ src/App.jsx
@@ -12,22 +12,76 @@ function App() {
 
 function Quiz(){
   const [quiz, setQuiz] = useState([]);
+  const [bcOnAns, setBcOnAns] = useState();
+
+  async function backendCall() {
+    const response = await fetch('http://localhost:3000/')
+    const res = await response.json(); 
+    setQuiz(res); 
+  }
 
   useEffect(()=>{
-    async function backendCall() {
-      const response = await fetch('http://localhost:3000/')
-      const res = await response.json();
-      setQuiz(res);
-    }
-
-    return ()=>{
-
-    }
+    backendCall()
   },[])
+
+  function onChangeHandler(event){
+    if(event.target.checked){
+      if(quiz[0].answer == event.target.value){
+        setBcOnAns('green');
+        event.target.backgroundColor = bcOnAns
+      }
+      else{
+        setBcOnAns('red');
+        event.target.backgroundColor = bcOnAns
+      }
+    }
+  }
 
   return (
     <>
-      {JSON.stringify(quiz)}
+    {quiz[0] &&
+      <div>
+        <h3>{quiz[0].question}</h3>
+
+        <label>
+          <input 
+            type='checkbox' 
+            value={quiz[0].opt1}
+            onChange={onChangeHandler}
+            />
+          {quiz[0].opt1}
+        </label>
+        <br/>
+        <label>
+          <input 
+            type='checkbox' 
+            value={quiz[0].opt2}
+            onChange={onChangeHandler}
+            />
+          {quiz[0].opt2}
+        </label>
+        <br/>
+        <label>
+          <input 
+            type='checkbox' 
+            value={quiz[0].opt3}
+            onChange={onChangeHandler}
+            />
+          {quiz[0].opt3}
+        </label>
+        <br/>
+        <label>
+          <input 
+            type='checkbox' 
+            value={quiz[0].opt4}
+            onChange={onChangeHandler}
+            />
+          {quiz[0].opt4}
+        </label>
+        <br/>
+        <button type='submit'>Next</button>
+      </div>
+    }
     </>
   )
 }
